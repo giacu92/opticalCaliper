14472793f4d2ab23a18d8d9e2a0dee8e2ffacf1c
Giacomo Mammarella <giacu92@gmail.com>
2017-01-23 18:08:06 +0100

    First commit
    Init git repository don’t use until beta release

---
+++ opticalCaliper.cpp
@@ -0,0 +1,73 @@
+//
+//  opticalCaliper.cpp
+//  opticalCaliper
+//
+//  Created by Giacomo Mammarella on 23/01/17.
+//  Copyright © 2017 Giacomo Mammarella. All rights reserved.
+//
+
+#include "opticalCaliper.h"
+
+opticalCaliper::opticalCaliper()
+{
+    pinMode(clockPin, OUTPUT);
+    pinMode(dataPin,  OUTPUT);
+    
+    this->clockPin = 4;
+    this->dataPin  = 5;
+    
+}
+
+opticalCaliper::opticalCaliper(int ckPin, int DataPin)
+{
+    opticalCaliper();
+    
+    this->clockPin = ckPin;
+    this->dataPin  = DataPin;
+}
+
+void opticalCaliper::setClockPin(int ckPin)
+{
+    this->clockPin = ckPin;
+}
+
+void opticalCaliper::setDataPin(int DataPin)
+{
+    this->dataPin = DataPin;
+}
+
+int opticalCaliper::getClockPin(void)
+{
+    return this->clockPin;
+}
+
+int opticalCaliper::getDataPin(void)
+{
+    return this->dataPin;
+}
+
+byte opticalCaliper::read(void)
+{
+    this->reading = 0; // azzeramanto
+    double tempMicros = micros();
+    
+    do{}while(digitalRead(clockPin) == LOW);
+    
+    if(micros() - tempMicros > 3000) // se mi trovo al primo impulso del treno avvio la lettura altrimenti salto
+    {
+        for (int i=0;i<=20;i++)
+        {
+            do{}while(digitalRead(clockPin) == HIGH);      // alla fine dell'impulso di clock leggo quello di data
+            if(digitalRead(dataPin) == 1)
+            {
+                bitSet(reading, i);
+            }
+            do{}while(digitalRead(clockPin) == LOW);       // attendo il prossimo impulso di clock
+        }
+        value2 = (494914 - reading) / 100.00 * 0.9922;
+    }  
+    else
+    {
+        read();
+    }
+}
---
+++ opticalCaliper.h
@@ -0,0 +1,39 @@
+//
+//  opticalCaliper.h
+//  opticalCaliper
+//
+//  Created by Giacomo Mammarella on 23/01/17.
+//  Copyright © 2017 Giacomo Mammarella. All rights reserved.
+//
+
+#ifndef opticalCaliper_h
+#define opticalCaliper_h
+
+#include "Arduino.h"
+
+#define const1 = 494914
+#define const2 = 99.22
+
+class opticalCaliper {
+
+private:
+    int clockPin = 0;   // default clockPin = 4
+    int dataPin  = 0;   // default dataPin  = 5
+    byte reading = 0x00;
+    
+public:
+    opticalCaliper();
+    opticalCaliper(int ckPin, int DataPin);
+    
+    void setClockPin(int ckPin  );
+    void setDataPin (int DataPin);
+    int  getClockPin(void);
+    int  getDataPin (void);
+    
+    byte read(void);        // returns the read byte
+    double mmRead(void);    // returns the read value in millimeters
+    double inRead(void);    // returns the read value in inches
+    
+}
+
+#endif /* opticalCaliper_h */
